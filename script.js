1e5cc62752a2a1c04d976aa0507299dca92f37d8
lizcory <evcory96@gmail.com>
2021-02-22 22:29:29 -0500

    updates office hours

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const size = {w: 250, h: 200};
 // contains the bar chart
 const yearSVG = d3.select('svg.year');
 // contains the coutnry map
-const mapSVG = d3.select('svg.delay');
+const mapSVG = d3.select('svg.map');
 
 // defining container groups
 const yearG = yearSVG.append('g').classed('container', true);
@@ -17,7 +17,7 @@ let netflixData,
     yearScaleX, yearScaleY,
     yearBrush, 
     filters = {},
-    dispatch = d3.dispatch('update');
+    dispatch = d3.dispatch('update'); // dispatch handles update events
 
 // setting width and height of the SVG elements
 yearSVG.attr('width', size.w)
@@ -37,9 +37,17 @@ Promise.all([
     console.log(mapData)
     console.log(netflixData)
 
-    // dispatch update data
-    dispatch.on('update', updateData);
-    dispatch.call('update');
+    // dispatch update data --
+    // dispatch.on('update', updateData); // when an update event happens, this is what function to call (updateData)
+    // dispatch.call('update');  // firing the event, making the update event happen
+
+
+    // trying to group the netflix data by country 
+    // will use for the choropleth to show num releases per country
+    let nestedCountryData = d3.group(netflixData, d => d.country);
+    nestedCountryData = Array.from(nestedCountryData);
+
+    console.log(nestedCountryData);
 
 
     // Adding year brush to the year bar-chart
@@ -58,17 +66,19 @@ Promise.all([
             let upperVal = yearScaleX.domain()[upperIndex]
 
             console.log(lowerVal, upperVal);
-            filters.time = [lowerVal, upperVal];
-            dispatch.call('update');
+            filters.releaseYear = [lowerVal, upperVal];
+            // dispatch.call('update');
+            updateData(); 
         });
     yearSVG.call(yearBrush);  // Binding the year brush to yearSVG
 
-    drawMap();
-    drawYearsChart();
+    let pathSel = drawMap(mapG, mapData);
+    drawYearsChart(netflixData);
+    choroplethizeMap(pathSel, nestedCountryData);
 
 });
 
-// Ensure data is updated
+// Ensure data is updated -- "wired" connection vs. dispatch "wireless"
 function updateData() {
 
     // Filtering out the data as per newly set filters
@@ -80,13 +90,18 @@ function updateData() {
             return d.releaseYear >= filters.releaseYear[0] && d.releaseYear <= filters.releaseYear[1];
         });
     }
-   
+
+    let nestedCountryData = d3.group(filteredData, d => d.country);
+    nestedCountryData = Array.from(nestedCountryData);
+
+    let pathSel = drawMap(mapG, mapData);
     drawYearsChart(filteredData);
+    choroplethizeMap(pathSel, nestedCountryData);
 
 }
 
 // DRAW BAR CHART for years
-function drawYearsChart(data = netflixData) {
+function drawYearsChart(data) {
 
     // data
     // x scale
@@ -94,18 +109,21 @@ function drawYearsChart(data = netflixData) {
     // draw bar shapes based on year
 
     if (!yearScaleX) {
+
+        let domain = [...Array(97).keys()];
+        domain = domain.map(d => d + 1925);
+
         yearScaleX = d3.scaleBand()
-            .domain([...Array(95).keys()])
-            .range([0, size.w])
+            .domain(domain) // data extent
+            .range([0, size.w]) // svg values
             .padding(0.2);
     }
+    // console.log(yearScaleX.domain());
 
     let nestedData = d3.group(data, d => d.releaseYear);
     nestedData = Array.from(nestedData);
 
-    console.log(nestedData);
-    console.log(nestedData[2]);
-
+    // console.log(nestedData);
 
     if (!yearScaleY) {
         yearScaleY = d3.scaleLinear()
@@ -117,8 +135,16 @@ function drawYearsChart(data = netflixData) {
         .data(nestedData)
         .join('rect')
         .attr('width', yearScaleX.bandwidth())
-        .attr('height', d => size.h - yearScaleY(d[1].length))
-        .attr('x', d => yearScaleX(d[0]))
+        // .attr('height', d => size.h - yearScaleY(d[1].length))
+        .attr('height', d => {
+            // console.log(d[1].length, yearScaleY(d[1].length))
+            return size.h - yearScaleY(d[1].length)
+        })
+        // .attr('x', d => yearScaleX(d[0]))
+        .attr('x', function(d) {
+            // console.log(d[0], yearScaleX(d[0]))
+            return yearScaleX(d[0]);
+        })
         .attr('y', d => yearScaleY(d[1].length))
 
 }
@@ -144,16 +170,10 @@ function drawMap(mapG, mapData) {
             return path(d);  
         });
 
-return pathSel; 
+    return pathSel; 
 
 }
 
-// trying to group the netflix data by country 
-// will use for the choropleth to show num releases per country
-let nestedCountryData = d3.group(netflixData, d => d.country);
-nestedCountryData = Array.from(nestedCountryData);
-
-console.log(nestedCountryData);
 
 // Make choropleth function to color the map
 function choroplethizeMap(pathSel, nestedCountryData) {
@@ -161,14 +181,14 @@ function choroplethizeMap(pathSel, nestedCountryData) {
     // make the color scale
     // update path selection function to include fill 
     // calculate color of each country based on color scale
-
+    console.log('CHOROPLETHIZE MAP')
     // make the color scale
     let extent = d3.extent(nestedCountryData, d => +d[1].length);
-    console.log(extent);
+    // console.log(extent);
 
     let colorScale = d3.scaleSequential()
         .domain(extent) // min/max of the netflix dataset's num releases/year
-        .interpolator(d3.interpolateRdYlBu); // need to convert 3D color space into single dimension
+        .interpolator(d3.interpolatePuOr); // need to convert 3D color space into single dimension
 
 
     // selecting the geographic data here with d
@@ -176,14 +196,16 @@ function choroplethizeMap(pathSel, nestedCountryData) {
 
     // here we are filtering to matching the names in both datasets, name (json) and country (netflixData)
     
-        let country = nestedCountryData.filter(ele => ele.country === d.properties.name);
+        let country = nestedCountryData.filter(ele => ele[0] === d.properties.name);
 
-        console.log(country); //array of objects
+        // console.log(d.properties);
+        // console.log(nestedCountryData[0]);
+
+        // console.log(country); //array of objects
 
         if (country.length > 0) {
             country = country[0];
-
-            return colorScale(country.length);
+            return colorScale(country[1].length);
         }
          return "#aaa";
     })
